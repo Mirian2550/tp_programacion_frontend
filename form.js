5b1a2f3f8e94ac770abb1cc53b7dc445984c3663
Fran <fjfalabella.94@gmail.com>
2025-08-21 18:04:03 -0300

    cambio en script formjs

--- form.js
+++ form.js
@@ -1,1 +1,9 @@
-// js
+const campoDirector = document.getElementById("director");
+const form = document.getElementById("movieForm");
+
+form.addEventListener("submit", function (event) {
+  if (/\d/.test(campoDirector.value)) {
+    alert("Ingresa un director/a válido");
+    event.preventDefault();
+  }
+});
