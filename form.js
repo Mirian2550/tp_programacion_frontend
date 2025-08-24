1b91af77cdd17c6ca288e8cd263c060225e0efad
miri <miri2550@gmail.com>
2025-08-24 16:11:24 -0300

    form con js

--- form.js
+++ form.js
@@ -1,10 +1,20 @@
-const myForm = document.getElementById("form");
-const inputDirector = document.getElementById("director");
-const errorElement = document.getElementById("error");
+const input = document.getElementById("plot");
+const remainingChars = document.getElementById("remaining-chars");
 
-myForm.addEventListener("submit", (e) => {
-  if (inputDirector.value === !NaN || inputDirector == null) {
-    console.log("El nombre del director/a es inválido");
+input.addEventListener("input", (e) => {
+  let len = e.target.value.length;
+  let remaining = 250 - len;
+
+  remainingChars.textContent = remaining;
+
+  remainingChars.className = "";
+  input.className = "";
+
+  if (len < 10) {
+    remainingChars.classList.add("error");
+    input.classList.add("error");
+  } else if (remaining <= 10) {
+    remainingChars.classList.add("warning");
+    input.classList.add("warning");
   }
-  e.preventDefault();
 });
