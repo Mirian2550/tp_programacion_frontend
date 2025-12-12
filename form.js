997ee47e9c80d3f0b0d7443d86ad6f94ec84ff96
BrisaGiavedoni <giavedonibrisa@gmail.com>
2025-12-12 17:25:53 -0300

    agregro codigo para agregar peli

--- form.js
+++ form.js
@@ -20,3 +20,47 @@ input.addEventListener("input", (e) => {
     input.classList.add("warning");
   }
 });
+//  Añadir pelicula 
+
+document.getElementById("movieForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const titulo = document.getElementById("title").value;
+  const anio = document.getElementById("year").value;
+  const genero = document.getElementById("genre").value;
+  const duracion = document.getElementById("duration").value;
+  const image_url = document.getElementById("imgUrl").value;
+  const idioma = document.getElementById("language").value;
+  const descripcion = document.getElementById("plot").value;
+
+  const body = {
+    titulo,
+    genero,
+    anio,
+    duracion,
+    image_url,
+    idioma,
+    descripcion,
+  };
+
+  try {
+    const res = await fetch("http://localhost:3000/movies", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      alert(data.message);
+      return;
+    }
+
+    alert("Película agregada correctamente");
+    window.location.href = "list.html";
+  } catch (err) {
+    alert("Error al conectar con el servidor");
+    console.error(err);
+  }
+});
